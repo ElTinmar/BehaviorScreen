178ee9f5ac3fa666957fd585a77a3e541c6bca27
ElTinmar <martin.privat@hotmail.fr>
2025-10-11 15:34:02 +0200

    added files

---
+++ BehaviorScreen/get_DLC_model.py
@@ -0,0 +1,4 @@
+# Plug-and-Play automated behavioral tracking of zebrafish larvae with DeepLabCut and SLEAP: pre-trained networks and datasets of annotated poses
+# https://doi.org/10.1101/2025.06.04.657938 
+
+DLC_MODELS_URL = "https://figshare.unimelb.edu.au/ndownloader/articles/29275838/versions/2?folder_path=DLC_exported-models"
