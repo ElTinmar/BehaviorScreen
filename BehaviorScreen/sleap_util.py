481323da32d7d1644c1e768b6f7942ebf48a331f
ElTinmar <martin.privat@hotmail.fr>
2025-10-17 00:25:28 +0200

    added replace nans

--- BehaviorScreen/sleap_util.py
+++ BehaviorScreen/sleap_util.py
@@ -1,5 +1,6 @@
 import sleap_io as sio
-from sleap import PredictedInstance, Labels
+from sleap import PredictedInstance
+import numpy as np
 
 def remove_all_predicted_instances(input_slp, output_slp):
     labels = sio.load_file(input_slp)
@@ -10,4 +11,15 @@ def remove_unlabeled_suggested_frames(input_slp, output_slp):
     labels = sio.load_file(input_slp)
     indices = [(f.video, f.frame_idx) for f in labels.labeled_frames]
     labels.suggestions = [s for s in labels.suggestions if (s.video, s.frame_idx) in indices]
+    labels.save(output_slp)
+
+def replace_nans(input_slp, output_slp):
+    labels = sio.load_file(input_slp)
+    for frame in labels.labeled_frames:
+        for inst in frame.instances:
+            points = inst.points['xy']
+            mask = np.isnan(points)
+            if np.any(mask):
+                print(inst)
+                points[mask] = 0
     labels.save(output_slp)
