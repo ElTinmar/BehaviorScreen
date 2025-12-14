8f712ca10fa850b4abe63978cb813f587ecd3a45
ElTinmar <martin.privat@hotmail.fr>
2025-12-14 18:59:38 +0100

    full_tracking

--- BehaviorScreen/megabouts.py
+++ BehaviorScreen/megabouts.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from cycler import cycler
@@ -75,6 +76,58 @@ def megabout_headtracking_pipeline(behavior_data: BehaviorData):
         ) 
 
     return megabout_results
+
+def full_tracking_pipeline(input_file, mm_per_px, fps):
+    
+    tracking_data = FullTrackingData_from_lp(input_file)
+    tracking_cfg = TrackingConfig(fps=fps, tracking="head_tracking")
+    pipeline = FullTrackingPipeline(tracking_cfg, exclude_CS=True)
+    pipeline.segmentation_cfg.min_bout_duration_ms=70
+    pipeline.segmentation_cfg.threshold = 7.5
+    pipeline.tail_preprocessing_cfg.tail_speed_boxcar_filter_ms = 30
+    pipeline.tail_preprocessing_cfg.savgol_window_ms = 20
+    ethogram, bouts, segments, tail, traj = pipeline.run(tracking_data)
+
+def FullTrackingData_from_sleap(
+        sleap_csv: str, 
+        mm_per_pix, fps, 
+        thresh_score: float = 0.75
+    ) -> FullTrackingData:
+    
+    df = pd.read_csv(lp_csv)
+    
+    head_x = df.heatmap_tracker.Head.x.values * mm_per_pix
+    head_y = df.heatmap_tracker.Head.y.values * mm_per_pix
+    tail_parts = [f"Tail_{i}" for i in range(9)] # exclude last tail point
+    tail_x = df.loc[:, ("heatmap_tracker", tail_parts, "x")].values * mm_per_pix
+    tail_y = df.loc[:, ("heatmap_tracker", tail_parts, "y")].values * mm_per_pix
+
+    tracking_cfg = TrackingConfig(fps=fps, tracking="full_tracking")
+    tracking_data = FullTrackingData.from_keypoints(
+        head_x=head_x, head_y=head_y, tail_x=tail_x, tail_y=tail_y
+    )
+    return tracking_data
+
+def FullTrackingData_from_lp(
+        lp_csv: str, 
+        mm_per_pix, 
+        fps, 
+        thresh_score: float = 0.75
+    ) -> FullTrackingData:
+    
+    df = pd.read_csv(lp_csv, header=[0,1,2])
+    
+    head_x = df.heatmap_tracker.Head.x.values * mm_per_pix
+    head_y = df.heatmap_tracker.Head.y.values * mm_per_pix
+    tail_parts = [f"Tail_{i}" for i in range(9)] # exclude last tail point
+    tail_x = df.loc[:, ("heatmap_tracker", tail_parts, "x")].values * mm_per_pix
+    tail_y = df.loc[:, ("heatmap_tracker", tail_parts, "y")].values * mm_per_pix
+
+    tracking_cfg = TrackingConfig(fps=fps, tracking="full_tracking")
+    tracking_data = FullTrackingData.from_keypoints(
+        head_x=head_x, head_y=head_y, tail_x=tail_x, tail_y=tail_y
+    )
+    return tracking_data
 
 def get_bout_metrics(
         directories: Directories,
@@ -475,105 +528,105 @@ def plot_ethogram(ethogram, bouts, IdSt, T):
 # pipeline.segmentation_cfg = TrajSegmentationConfig(fps=tracking_cfg.fps)
 # ethogram, bouts, segments, tail, traj = pipeline.run(tracking_data)
 
-# # Number of bouts as function of threshold:
-# thresh_list = np.linspace(0.1, 5, 100)
-# num_bouts = np.zeros_like(thresh_list)
+# Number of bouts as function of threshold:
+thresh_list = np.linspace(0.01, 1, 100)
+num_bouts = np.zeros_like(thresh_list)
 
-# for i, thresh in enumerate(thresh_list):
-#     traj_segmentation_cfg = TrajSegmentationConfig(
-#         fps=tracking_cfg.fps, peak_prominence=thresh
-#     )
-#     segmentation_function = Segmentation.from_config(traj_segmentation_cfg)
-#     segments = segmentation_function.segment(traj.vigor)
-#     num_bouts[i] = len(segments.onset)
+for i, thresh in enumerate(thresh_list):
+    traj_segmentation_cfg = TrajSegmentationConfig(
+        fps=tracking_cfg.fps, peak_prominence=thresh
+    )
+    segmentation_function = Segmentation.from_config(traj_segmentation_cfg)
+    segments = segmentation_function.segment(traj.vigor)
+    num_bouts[i] = len(segments.onset)
 
-# fig, ax = plt.subplots(figsize=(6, 4))
-# ax.plot(thresh_list, num_bouts)
-# plt.show()
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.plot(thresh_list, num_bouts)
+plt.show()
 
-# # plot classification
-# id_b = np.unique(bouts.df.label.category[bouts.df.label.proba > 0.5]).astype("int")
+# plot classification
+id_b = np.unique(bouts.df.label.category[bouts.df.label.proba > 0.5]).astype("int")
 
-# fig, ax = plt.subplots(facecolor="white", figsize=(25, 4))
+fig, ax = plt.subplots(facecolor="white", figsize=(25, 4))
 
-# ax.spines["top"].set_visible(False)
-# ax.spines["right"].set_visible(False)
-# ax.spines["bottom"].set_visible(False)
-# ax.spines["left"].set_visible(False)
-# ax.set_xticks([])
-# ax.set_yticks([])
-# G = gridspec.GridSpec(1, len(id_b))
-# ax0 = {}
-# for i, b in enumerate(id_b):
-#     ax0 = plt.subplot(G[i])
-#     ax0.set_title(bouts_category_name_short[b], fontsize=15)
-#     for i_sg, sg in enumerate([1, -1]):
-#         id = bouts.df[
-#             (bouts.df.label.category == b)
-#             & (bouts.df.label.sign == sg)
-#             & (bouts.df.label.proba > 0.5)
-#         ].index
-#         if len(id) > 0:
-#             ax0.plot(sg * bouts.tail[id, 7, :].T, color="k", alpha=0.3)
-#         ax0.set_xlim(0, pipeline.segmentation_cfg.bout_duration)
-#         ax0.set_ylim(-4, 4)
-#         ax0.set_xticks([])
-#         ax0.set_yticks([])
-#         for sp in ["top", "bottom", "left", "right"]:
-#             ax0.spines[sp].set_color(bouts_category_color[b])
-#             ax0.spines[sp].set_linewidth(5)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.set_xticks([])
+ax.set_yticks([])
+G = gridspec.GridSpec(1, len(id_b))
+ax0 = {}
+for i, b in enumerate(id_b):
+    ax0 = plt.subplot(G[i])
+    ax0.set_title(bouts_category_name_short[b], fontsize=15)
+    for i_sg, sg in enumerate([1, -1]):
+        id = bouts.df[
+            (bouts.df.label.category == b)
+            & (bouts.df.label.sign == sg)
+            & (bouts.df.label.proba > 0.5)
+        ].index
+        if len(id) > 0:
+            ax0.plot(sg * bouts.tail[id, 7, :].T, color="k", alpha=0.3)
+        ax0.set_xlim(0, pipeline.segmentation_cfg.bout_duration)
+        ax0.set_ylim(-4, 4)
+        ax0.set_xticks([])
+        ax0.set_yticks([])
+        for sp in ["top", "bottom", "left", "right"]:
+            ax0.spines[sp].set_color(bouts_category_color[b])
+            ax0.spines[sp].set_linewidth(5)
 
-# plt.show()
+plt.show()
 
-# IdSt = 0
-# T = 100
-# Duration = T * tracking_cfg.fps
-# IdEd = IdSt + Duration
-# t = np.arange(Duration) / tracking_cfg.fps
+IdSt = 0
+T = 100
+Duration = T * tracking_cfg.fps
+IdEd = IdSt + Duration
+t = np.arange(Duration) / tracking_cfg.fps
 
 
-# fig = plt.figure(facecolor="white", figsize=(15, 5), constrained_layout=True)
-# G = gridspec.GridSpec(2, 1, height_ratios=[1, 0.2], hspace=0.5, figure=fig)
-# ax = plt.subplot(G[0, 0])
-# blue_cycler = cycler(color=plt.cm.Blues(np.linspace(0.2, 0.9, 10)))
-# ax.set_prop_cycle(blue_cycler)
+fig = plt.figure(facecolor="white", figsize=(15, 5), constrained_layout=True)
+G = gridspec.GridSpec(2, 1, height_ratios=[1, 0.2], hspace=0.5, figure=fig)
+ax = plt.subplot(G[0, 0])
+blue_cycler = cycler(color=plt.cm.Blues(np.linspace(0.2, 0.9, 10)))
+ax.set_prop_cycle(blue_cycler)
 
-# ax.plot(t, ethogram.df["tail_angle"].values[IdSt:IdEd, :7], lw=1)
-# ax.set_ylim(-4, 4)
-# ax.set_xlim(0, T)
+ax.plot(t, ethogram.df["tail_angle"].values[IdSt:IdEd, :7], lw=1)
+ax.set_ylim(-4, 4)
+ax.set_xlim(0, T)
 
-# ax.spines["top"].set_visible(False)
-# ax.spines["right"].set_visible(False)
-# ax.spines["bottom"].set_visible(False)
-# ax.get_yaxis().tick_left()
-# ax.get_xaxis().set_ticks([])
-# ax.set_ylabel("tail angle (rad)", rotation=0, labelpad=100)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.get_yaxis().tick_left()
+ax.get_xaxis().set_ticks([])
+ax.set_ylabel("tail angle (rad)", rotation=0, labelpad=100)
 
-# ax = plt.subplot(G[1, 0])
-# ax.imshow(
-#     ethogram.df[("bout", "cat")].values[IdSt:IdEd].T,
-#     cmap=cmp_bouts,
-#     aspect="auto",
-#     vmin=0,
-#     vmax=12,
-#     interpolation="nearest",
-#     extent=(0, T, 0, 1),
-# )
-# ax.spines["top"].set_visible(False)
-# ax.spines["right"].set_visible(False)
-# ax.spines["bottom"].set_visible(False)
-# ax.get_yaxis().tick_left()
-# ax.get_xaxis().set_ticks([])
-# ax.get_yaxis().set_ticks([])
-# ax.set_xlim(0, T)
-# ax.set_ylim(0, 1.1)
+ax = plt.subplot(G[1, 0])
+ax.imshow(
+    ethogram.df[("bout", "cat")].values[IdSt:IdEd].T,
+    cmap=cmp_bouts,
+    aspect="auto",
+    vmin=0,
+    vmax=12,
+    interpolation="nearest",
+    extent=(0, T, 0, 1),
+)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.get_yaxis().tick_left()
+ax.get_xaxis().set_ticks([])
+ax.get_yaxis().set_ticks([])
+ax.set_xlim(0, T)
+ax.set_ylim(0, 1.1)
 
-# id_b = np.unique(ethogram.df[("bout", "id")].values[IdSt:IdEd]).astype("int")
-# id_b = id_b[id_b > -1]
-# for i in id_b:
-#     on_ = bouts.df.iloc[i][("location", "onset")]
-#     b = bouts.df.iloc[i][("label", "category")]
-#     ax.text((on_ - IdSt) / tracking_cfg.fps, 1.1, bouts_category_name[int(b)])
+id_b = np.unique(ethogram.df[("bout", "id")].values[IdSt:IdEd]).astype("int")
+id_b = id_b[id_b > -1]
+for i in id_b:
+    on_ = bouts.df.iloc[i][("location", "onset")]
+    b = bouts.df.iloc[i][("label", "category")]
+    ax.text((on_ - IdSt) / tracking_cfg.fps, 1.1, bouts_category_name[int(b)])
 
-# ax.set_ylabel("bout category", rotation=0, labelpad=100)
-# plt.show()
+ax.set_ylabel("bout category", rotation=0, labelpad=100)
+plt.show()
