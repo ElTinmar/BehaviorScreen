0379f3d073fa771bd1d550b6fc70f5a92d838eee
ElTinmar <martin.privat@hotmail.fr>
2025-12-19 13:41:47 +0100

    capital

--- BehaviorScreen/config.py
+++ BehaviorScreen/config.py
@@ -15,7 +15,7 @@ NUM_PROCESSES = default_num_processes()
 HOST = socket.gethostname()
 
 DATA_ROOTS = {
-    "O1-596": Path("/media/martin/DATA1/Behavioral_screen"),
+    "o1-596": Path("/media/martin/DATA1/Behavioral_screen"),
     "O1-619": Path("/home/martin/Desktop/DATA"),
     "TheBeast": Path("/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen"),
 }
