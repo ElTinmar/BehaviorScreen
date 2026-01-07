e17f7fb410bf2802611d5875a7e4cca5c82c7ba6
ElTinmar <martin.privat@hotmail.fr>
2026-01-07 12:09:11 +0100

    functions

--- BehaviorScreen/__main__.py
+++ BehaviorScreen/__main__.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional, Iterable, Sequence
+from typing import List, Dict, Optional, Iterable, Sequence, Tuple
 import os
 from multiprocessing import Pool
 from functools import partial
@@ -1363,15 +1363,6 @@ for i in range(25):
 
 ###
 
-stimuli = {
-    Stim.PREY_CAPTURE: ['-20', '20'],
-    Stim.PHOTOTAXIS: ['-1','1'],
-    Stim.OMR: ['-90', '90', '0'],
-    Stim.OKR: ['-36', '36'],
-    Stim.LOOMING: ['-3', '3']
-}
-
-# time bins (s)
 time_bins = [
     (0, 2.5),
     (2.5, 5),
@@ -1382,97 +1373,124 @@ time_bins = [
     (20, 30),
 ]
 
-sides = ['L', 'R']
-num_cat = len(bouts_category_name_short)
-row_labels = [f"{cat}_{side}" for cat in bouts_category_name_short for side in sides]
+def get_bouts_heatmap(
+    bouts: pd.DataFrame,
+    bouts_category_name_short: List[str] = bouts_category_name_short,
+    time_bins: List[Tuple[float, float]] = time_bins,
+) -> pd.DataFrame:
 
-heatmap_df = pd.DataFrame()
-
-epochs = {}
-
-# DARK
-for start, stop in time_bins:
-    name = f"DARK_{start}-{stop}s"
-    epochs[name] = (
-        (bouts.stim == Stim.DARK) &
-        (bouts.trial_num >= 10) &
-        (bouts.trial_num < 20) &
-        (bouts.trial_time >= start) &
-        (bouts.trial_time <= stop)
-    )
+    stimuli = {
+        Stim.PREY_CAPTURE: ['-20', '20'],
+        Stim.PHOTOTAXIS: ['-1','1'],
+        Stim.OMR: ['-90', '90', '0'],
+        Stim.OKR: ['-36', '36'],
+        Stim.LOOMING: ['-3', '3']
+    }
     
-# BRIGHT
-for start, stop in time_bins:
-    name = f"BRIGHT_{start}-{stop}s"
-    epochs[name] = (
-        (bouts.stim == Stim.BRIGHT) &
-        (bouts.stim_variable_value == '[0.2, 0.2, 0.0, 1.0]') &
-        (bouts.trial_num >= 5) &
-        (bouts.trial_time >= start) &
-        (bouts.trial_time <= stop)
-    )
+    sides = ['L', 'R']
+    num_cat = len(bouts_category_name_short)
+    row_labels = [f"{cat}_{side}" for cat in bouts_category_name_short for side in sides]
 
-for stim, param_list in stimuli.items():
+    heatmap_df = pd.DataFrame()
+
+    epochs = {}
+
+    # DARK
     for start, stop in time_bins:
-        for p in param_list:
+        name = f"DARK_{start}-{stop}s"
+        epochs[name] = (
+            (bouts.stim == Stim.DARK) &
+            (bouts.trial_num >= 10) &
+            (bouts.trial_num < 20) &
+            (bouts.trial_time >= start) &
+            (bouts.trial_time <= stop)
+        )
+        
+    # BRIGHT
+    for start, stop in time_bins:
+        name = f"BRIGHT_{start}-{stop}s"
+        epochs[name] = (
+            (bouts.stim == Stim.BRIGHT) &
+            (bouts.stim_variable_value == '[0.2, 0.2, 0.0, 1.0]') &
+            (bouts.trial_num >= 5) &
+            (bouts.trial_time >= start) &
+            (bouts.trial_time <= stop)
+        )
 
-            if stim in {Stim.OKR, Stim.OMR, Stim.LOOMING} and start >= 10:
-                continue
-            if stim is Stim.PHOTOTAXIS and start >= 5:
-                continue
+    for stim, param_list in stimuli.items():
+        for start, stop in time_bins:
+            for p in param_list:
 
-            name = f"{stim.name}_{p}_{start}-{stop}s"
+                if stim in {Stim.OKR, Stim.OMR, Stim.LOOMING} and start >= 10:
+                    continue
+                if stim is Stim.PHOTOTAXIS and start >= 5:
+                    continue
 
-            epochs[name] = (
-                (bouts.stim == stim) &
-                (bouts.stim_variable_value == p) &
-                (bouts.trial_time >= start) &
-                (bouts.trial_time <= stop)
-            )
+                name = f"{stim.name}_{p}_{start}-{stop}s"
 
-# O-BEND
-for start, stop in time_bins:
-    if start >= 5:
-        break
+                epochs[name] = (
+                    (bouts.stim == stim) &
+                    (bouts.stim_variable_value == p) &
+                    (bouts.trial_time >= start) &
+                    (bouts.trial_time <= stop)
+                )
 
-    name = f"BRIGHT->DARK_{start}-{stop}s"
-    epochs[name] = (
-        (bouts.stim == Stim.DARK) &
-        (bouts.trial_num >= 20) &
-        (bouts.trial_num < 25) &
-        (bouts.trial_time >= start) &
-        (bouts.trial_time <= stop)
-    )
+    # O-BEND
+    for start, stop in time_bins:
+        if start >= 5:
+            break
 
-for name, mask in epochs.items():
+        name = f"BRIGHT->DARK_{start}-{stop}s"
+        epochs[name] = (
+            (bouts.stim == Stim.DARK) &
+            (bouts.trial_num >= 20) &
+            (bouts.trial_num < 25) &
+            (bouts.trial_time >= start) &
+            (bouts.trial_time <= stop)
+        )
 
-    df_sub = bouts[
-        mask &
-        (bouts.proba > 0.5) &
-        (bouts.distance_center < 15)
-    ]
+    for name, mask in epochs.items():
 
-    counts = []
-    for cat in range(num_cat):
-        left = df_sub[(df_sub.category == cat) & (df_sub.sign == -1)].shape[0]
-        right = df_sub[(df_sub.category == cat) & (df_sub.sign == 1)].shape[0]
-        counts.extend([left, right])
+        df_sub = bouts[
+            mask &
+            (bouts.proba > 0.5) &
+            (bouts.distance_center < 15)
+        ]
 
-    counts = pd.Series(counts, index=row_labels)
-    if counts.sum() > 0:
-        counts /= counts.sum()
+        counts = []
+        for cat in range(num_cat):
+            left = df_sub[(df_sub.category == cat) & (df_sub.sign == -1)].shape[0]
+            right = df_sub[(df_sub.category == cat) & (df_sub.sign == 1)].shape[0]
+            counts.extend([left, right])
 
-    heatmap_df[name] = counts
+        counts = pd.Series(counts, index=row_labels)
+        if counts.sum() > 0:
+            counts /= counts.sum()
 
-plt.figure(figsize=(20, 8))
-plt.imshow(heatmap_df, aspect='auto', cmap='inferno')
-plt.colorbar(label='prob.')
+        heatmap_df[name] = counts
+    
+    return heatmap_df
 
-plt.xticks(range(len(heatmap_df.columns)), heatmap_df.columns, rotation=90, ha='center')
-plt.yticks(range(len(heatmap_df.index)), heatmap_df.index)
 
-plt.xlabel("Epoch")
-plt.ylabel("Category")
-plt.tight_layout()
+def plot_bout_heatmap(fig, ax, heatmap_df) -> None:
+
+    im = ax.imshow(heatmap_df, aspect='auto', cmap='inferno')
+    fig.colorbar(im, ax=ax, label='prob.')
+
+    ax.set_xticks(range(len(heatmap_df.columns)))
+    ax.set_xticklabels(heatmap_df.columns, rotation=90, ha='center')
+
+    ax.set_yticks(range(len(heatmap_df.index)))
+    ax.set_yticklabels(heatmap_df.index)
+
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Category")
+
+
+heatmap_df = get_bouts_heatmap(bouts)
+fig = plt.figure(figsize=(20, 8))
+ax = fig.gca()
+plot_bout_heatmap(fig, ax, heatmap_df)
+fig.tight_layout()
 plt.savefig("categories_vs_time.png")
 plt.show()
