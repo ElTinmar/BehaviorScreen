2bd68a4e96b03bd14eadd2fa5ba1a2d6594a6f4f
ElTinmar <martin.privat@hotmail.fr>
2026-02-09 11:44:11 +0100

    python instead of bash

---
+++ scripts/local/config.py
@@ -0,0 +1,27 @@
+ROOT = '/media/martin/DATA1/Behavioral_screen/DATA'
+
+FOLDERS = [
+    '1010Kaede-X-81C/ronidazole',
+    '1010Kaede-X-81C/vehicle',
+    'ccka/ccka-ronidazole',
+    'ccka/ccka-vehicle',
+    'cort/cort-ronidazole',
+    'cort/cort-vehicle',
+    'itpr1b/ronidazole',
+    'itpr1b/vehicle',
+    'lak',
+    'mafaa/mafaa-ronidazole',
+    'mafaa/mafaa-vehicle',
+    'pmch2/ronidazole',
+    'pmch2/vehicle',
+    'pmchl/ronidazole',
+    'pmchl/vehicle',
+    'pth2/pth2-ronidazole',
+    'pth2/pth2-vehicle',
+    'rspo1/ronidazole',
+    'rspo1/vehicle',
+    'tbr1b/ronidazole',
+    'tbr1b/vehicle',
+    'WT/danieau',
+    'WT/ronidazole'
+] 
