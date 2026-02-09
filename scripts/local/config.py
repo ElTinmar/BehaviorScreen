702915265e08a60624bc52d534ae18ab4b284260
ElTinmar <martin.privat@hotmail.fr>
2026-02-09 11:53:09 +0100

    pathlib

--- scripts/local/config.py
+++ scripts/local/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT = Path('/media/martin/DATA1/Behavioral_screen/DATA')
 
-CONFIG_YAML = 'BehaviorScreen/screen.yaml'
+CONFIG_YAML = Path('BehaviorScreen/screen.yaml')
 
 FOLDERS = [
     '1010Kaede-X-81C/ronidazole',
