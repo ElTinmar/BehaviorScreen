8507c9296d5ea1a402162839ca1d5793318c4ac8
ElTinmar <martin.privat@hotmail.fr>
2026-02-17 13:05:31 +0100

    line skip

--- BehaviorScreen/mixed_model.R
+++ BehaviorScreen/mixed_model.R
@@ -3,6 +3,8 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
+bout_category_levels = c()
+
 #data <- read_csv("/home/martin/Desktop/bouts/WT/danieau/bout_frequency.csv")
 data <- read_csv("/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen/DATA/WT/danieau/bout_frequency.csv")
 
@@ -10,7 +12,7 @@ data <- data %>%
   mutate(
     fish = factor(fish),
     day = factor(day),
-    bout_category = factor(bout_category),
+    bout_category = factor(bout_category, levels=bout_category_levels),
     bout_side = factor(bout_side),
     epoch_name = factor(epoch_name),
     stim_param = factor(stim_param)
@@ -47,9 +49,13 @@ anova(model)
 
 # trial time
 ggplot(data, aes(x = trial_time, y = bout_frequency, color = bout_category)) +
-  geom_point() + geom_jitter() + facet_grid(epoch_name*stim_param~bout_category*bout_side) 
+  geom_point() + 
+  geom_jitter() + 
+  facet_grid(epoch_name*stim_param~bout_category*bout_side) 
 
 # trial num
 ggplot(data, aes(x = trial_num, y = bout_frequency, color = bout_category)) +
-  geom_point() + geom_jitter() + facet_grid(epoch_name*stim_param~bout_category*bout_side) 
+  geom_point() + 
+  geom_jitter() + 
+  facet_grid(epoch_name*stim_param~bout_category*bout_side) 
 
