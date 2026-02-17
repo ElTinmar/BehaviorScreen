92aef292f47da14c1a793d140f5a9f823e04087f
ElTinmar <martin.privat@hotmail.fr>
2026-02-17 10:41:07 +0100

    multiprocessed local plot

--- scripts/local/config.py
+++ scripts/local/config.py
@@ -1,31 +1,35 @@
 from pathlib import Path
 
-ROOT = Path('/media/martin/DATA1/Behavioral_screen/DATA')
+#ROOT = Path('/media/martin/DATA1/Behavioral_screen/DATA')
+ROOT = Path('/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen/DATA')
+
 
 CONFIG_YAML = Path('BehaviorScreen/screen.yaml')
 
 FOLDERS = [
     '1010Kaede-X-81C/ronidazole',
     '1010Kaede-X-81C/vehicle',
-    'ccka/ccka-ronidazole',
-    'ccka/ccka-vehicle',
-    'cort/cort-ronidazole',
-    'cort/cort-vehicle',
+    'ccka/ronidazole',
+    'ccka/vehicle',
+    'cort/ronidazole',
+    'cort/vehicle',
     'itpr1b/ronidazole',
     'itpr1b/vehicle',
-    'lak',
-    'mafaa/mafaa-ronidazole',
-    'mafaa/mafaa-vehicle',
+    'lak/danieau',
+    'mafaa/ronidazole',
+    'mafaa/vehicle',
     'pmch2/ronidazole',
     'pmch2/vehicle',
     'pmchl/ronidazole',
     'pmchl/vehicle',
-    'pth2/pth2-ronidazole',
-    'pth2/pth2-vehicle',
+    'pth2/ronidazole',
+    'pth2/vehicle',
     'rspo1/ronidazole',
     'rspo1/vehicle',
     'tbr1b/ronidazole',
     'tbr1b/vehicle',
     'WT/danieau',
-    'WT/ronidazole'
+    'WT/ronidazole',
+    'atf5b/ronidazole',
+    'atf5b/vehicle',
 ] 
--- scripts/local/plot.py
+++ scripts/local/plot.py
@@ -1,9 +1,16 @@
 from .config import ROOT, FOLDERS, CONFIG_YAML
 from BehaviorScreen.plot import plot_heatmap
+from multiprocessing import Pool, cpu_count
 
-for f in FOLDERS:
-    target = ROOT / f
+def process_folder(folder):
+    target = ROOT / folder
     print(f"processing {target}")
+
     input_csv = target / "bouts.csv"
     output_png = target / "bouts.png"
+
     plot_heatmap(input_csv, CONFIG_YAML, output_png)
+    
+if __name__ == "__main__":
+    with Pool(cpu_count()) as pool:
+        pool.map(process_folder, FOLDERS)
