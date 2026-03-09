b04f07a473588c73049dcb0a3f642c5da2f665f1
ElTinmar <martin.privat@hotmail.fr>
2026-03-09 22:10:52 +0100

    reorganize

--- BehaviorScreen/playground.py
+++ BehaviorScreen/playground.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+from typing import List
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 from BehaviorScreen.load import (
     Directories, 
     BehaviorData,
@@ -5,12 +12,48 @@ from BehaviorScreen.load import (
     find_files, 
     load_data
 )
-from pathlib import Path
-from typing import List
-import pickle
 from BehaviorScreen.megabouts import MegaboutResults
-from BehaviorScreen.process import get_trials
+from BehaviorScreen.process import get_trials, compute_angle_between_vectors
 from BehaviorScreen.core import Stim, GROUPING_PARAMETER
+
+def get_eye_traces(
+        data: pd.DataFrame, 
+        likelihood_threshold: float = 0.9,
+        divergence_threshold_deg: float = -10,
+        convergence_threshold_deg: float = 60
+    ) -> tuple[np.ndarray, np.ndarray]:
+
+    # extract data
+    left_front_keypoint = data.eye_left_front[['x', 'y']].to_numpy()
+    left_front_likelihood = data.eye_left_front.likelihood.to_numpy()
+
+    left_back_keypoint = data.eye_left_back[['x', 'y']].to_numpy()
+    left_back_likelihood = data.eye_left_back.likelihood.to_numpy()
+
+    right_front_keypoint = data.eye_right_front[['x', 'y']].to_numpy()
+    right_front_likelihood = data.eye_right_front.likelihood.to_numpy()
+
+    right_back_keypoint = data.eye_right_back[['x', 'y']].to_numpy()
+    right_back_likelihood = data.eye_right_back.likelihood.to_numpy()
+
+    left_vector = left_back_keypoint - left_front_keypoint
+    right_vector = right_back_keypoint - right_front_keypoint
+
+    L_rad = compute_angle_between_vectors(left_vector, np.array([0,1]))
+    R_rad = compute_angle_between_vectors(right_vector, np.array([0,1]))
+    L = np.rad2deg(L_rad)
+    R = np.rad2deg(R_rad)
+
+    L[(left_front_likelihood < likelihood_threshold) | (left_back_likelihood < likelihood_threshold)] = np.nan
+    R[(right_front_likelihood < likelihood_threshold) | (right_back_likelihood < likelihood_threshold)] = np.nan
+    L[(-L<divergence_threshold_deg) & (-L>convergence_threshold_deg)] = np.nan
+    R[(R<divergence_threshold_deg) & (R>convergence_threshold_deg)] = np.nan
+
+    L = pd.Series(L).interpolate(limit_direction="both").to_numpy()
+    R = pd.Series(R).interpolate(limit_direction="both").to_numpy()
+
+    return L, R
+
 
 ROOT = Path('/media/martin/DATA_18TB/Screen/WT/danieau')
 ROOT = Path('/media/martin/DATA/Behavioral_screen/DATA/Screen/WT/danieau')
@@ -64,52 +107,8 @@ for stim_select, stim_data in stim_trials.groupby('stim_select'):
 
 # =============================================================================
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from BehaviorScreen.process import compute_angle_between_vectors
-from scipy.signal import savgol_filter
-from pathlib import Path
-
 #filename = '/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen/eye_models/current/01_07dpf_WT_Thu_11_Dec_2025_13h15min29sec_fish_3_eyes.csv'
 
-def get_eye_traces(
-        data: pd.DataFrame, 
-        likelihood_threshold: float = 0.9,
-        divergence_threshold_deg: float = -10,
-        convergence_threshold_deg: float = 60
-    ) -> tuple[np.ndarray, np.ndarray]:
-
-    # extract data
-    left_front_keypoint = data.eye_left_front[['x', 'y']].to_numpy()
-    left_front_likelihood = data.eye_left_front.likelihood.to_numpy()
-
-    left_back_keypoint = data.eye_left_back[['x', 'y']].to_numpy()
-    left_back_likelihood = data.eye_left_back.likelihood.to_numpy()
-
-    right_front_keypoint = data.eye_right_front[['x', 'y']].to_numpy()
-    right_front_likelihood = data.eye_right_front.likelihood.to_numpy()
-
-    right_back_keypoint = data.eye_right_back[['x', 'y']].to_numpy()
-    right_back_likelihood = data.eye_right_back.likelihood.to_numpy()
-
-    left_vector = left_back_keypoint - left_front_keypoint
-    right_vector = right_back_keypoint - right_front_keypoint
-
-    L_rad = compute_angle_between_vectors(left_vector, np.array([0,1]))
-    R_rad = compute_angle_between_vectors(right_vector, np.array([0,1]))
-    L = np.rad2deg(L_rad)
-    R = np.rad2deg(R_rad)
-
-    L[(left_front_likelihood < likelihood_threshold) | (left_back_likelihood < likelihood_threshold)] = np.nan
-    R[(right_front_likelihood < likelihood_threshold) | (right_back_likelihood < likelihood_threshold)] = np.nan
-    L[(-L<divergence_threshold_deg) & (-L>convergence_threshold_deg)] = np.nan
-    R[(R<divergence_threshold_deg) & (R>convergence_threshold_deg)] = np.nan
-
-    L = pd.Series(L).interpolate(limit_direction="both").to_numpy()
-    R = pd.Series(R).interpolate(limit_direction="both").to_numpy()
-
-    return L, R
 
 fs = 120
 BASE_DIR = '/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen/eye_models/current'
