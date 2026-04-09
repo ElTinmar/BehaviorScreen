6b84f3b3444b8156d4a01f63a9a0b26c89f4de0c
ElTinmar <martin.privat@hotmail.fr>
2026-04-09 15:28:09 +0200

    update plots

--- BehaviorScreen/plot_mecp2.py
+++ BehaviorScreen/plot_mecp2.py
@@ -21,7 +21,8 @@ plt.rcParams.update({
     'ytick.labelsize': 14,    # Y tick labels
     'legend.fontsize': 12,    # Legend
     'font.family': 'sans-serif',
-    'font.sans-serif': ['Arial'] # Common for biology journals
+    'font.sans-serif': ['Arial'], 
+    'axes.linewidth': 1.5
 })
 
 ROOT = Path('/home/martin/Desktop/DATA')
@@ -52,7 +53,8 @@ ipsilateral = [(BoutSign.LEFT, PreySide.LEFT), (BoutSign.RIGHT, PreySide.RIGHT)]
 contralateral = [(BoutSign.LEFT, PreySide.RIGHT), (BoutSign.RIGHT, PreySide.LEFT)]
 laterality = [ipsilateral, contralateral]
 
-heatmap = np.full((len(groups), N_fish, len(laterality), N_trials, len(time_bins)), np.nan, dtype=np.float32)
+JT_freq = np.full((len(groups), N_fish, len(laterality), N_trials, len(time_bins)), np.nan, dtype=np.float32)
+JT_count = np.full((len(groups), N_fish, len(laterality), N_trials, len(time_bins)), np.nan, dtype=np.float32)
 
 for g_idx, g in enumerate(groups):
 
@@ -76,7 +78,8 @@ for g_idx, g in enumerate(groups):
                             (df.sign == bout_sign) & 
                             (df.prey_arc_start_deg == prey_side)
                         )
-                        heatmap[g_idx, fish_idx, lat_idx, trial, bin_idx] = mask.sum() / (t_stop - t_start)
+                        JT_freq[g_idx, fish_idx, lat_idx, trial, bin_idx] = mask.sum() / (t_stop - t_start)
+                        JT_count[g_idx, fish_idx, lat_idx, trial, bin_idx] = mask.sum()
 
 
 group_names = ['Mecp2', 'WT']
@@ -93,7 +96,7 @@ fig, axes = plt.subplots(len(group_names), len(lat_names),
 for g_idx in range(len(group_names)):
     for lat_idx in range(len(lat_names)):
         ax = axes[g_idx, lat_idx]
-        data = np.nanmean(heatmap[g_idx, :,lat_idx, :, :], axis=0)
+        data = np.nanmean(JT_freq[g_idx, :,lat_idx, :, :], axis=0)
         
         sns.heatmap(data, 
                     annot=True,       
@@ -119,15 +122,20 @@ plt.show()
 ### German's barplot
 
 data_dict = {
-    'Mecp2_Ipsi':   np.nanmean(heatmap[0, :, 0, 0:3, 0], axis=1),
-    'Mecp2_Contra': np.nanmean(heatmap[0, :, 1, 0:3, 0], axis=1),
-    'WT_Ipsi':      np.nanmean(heatmap[1, :, 0, 0:3, 0], axis=1),
-    'WT_Contra':    np.nanmean(heatmap[1, :, 1, 0:3, 0], axis=1)
+    'Mecp2_Ipsi':   np.nanmean(JT_freq[0, :, 0, 0:3, 0], axis=1),
+    'Mecp2_Contra': np.nanmean(JT_freq[0, :, 1, 0:3, 0], axis=1),
+    'WT_Ipsi':      np.nanmean(JT_freq[1, :, 0, 0:3, 0], axis=1),
+    'WT_Contra':    np.nanmean(JT_freq[1, :, 1, 0:3, 0], axis=1)
 }
 
 p_ipsi_between = mannwhitneyu(
     data_dict['Mecp2_Ipsi'][~np.isnan(data_dict['Mecp2_Ipsi'])],
     data_dict['WT_Ipsi'][~np.isnan(data_dict['WT_Ipsi'])]
+).pvalue
+
+p_contra_between = mannwhitneyu(
+    data_dict['Mecp2_Contra'][~np.isnan(data_dict['Mecp2_Contra'])],
+    data_dict['WT_Contra'][~np.isnan(data_dict['WT_Contra'])]
 ).pvalue
 
 # Mecp2 (paired)
@@ -144,22 +152,22 @@ p_wt = wilcoxon(
     data_dict['WT_Contra'][mask_w]
 ).pvalue
 
-pvals = (p_ipsi_between, p_mecp2, p_wt)
-rejected, (p_ipsi_between_bf, p_mecp2_bf, p_wt_bf), _, _ = multipletests(pvals, alpha=0.05, method='bonferroni')
+pvals = (p_ipsi_between, p_contra_between, p_mecp2, p_wt)
+rejected, (p_ipsi_between_bf, p_contra_between_bf, p_mecp2_bf, p_wt_bf), _, _ = multipletests(pvals, alpha=0.05, method='bonferroni')
 
 df_plot = pd.DataFrame({
     'value': np.concatenate(list(data_dict.values())),
     'group': (
-        ['Mecp2'] * len(data_dict['Mecp2_Ipsi']) +
-        ['Mecp2'] * len(data_dict['Mecp2_Contra']) +
-        ['WT']    * len(data_dict['WT_Ipsi']) +
-        ['WT']    * len(data_dict['WT_Contra'])
+        ['mecp2-mutant'] * len(data_dict['Mecp2_Ipsi']) +
+        ['mecp2-mutant'] * len(data_dict['Mecp2_Contra']) +
+        ['wild type']    * len(data_dict['WT_Ipsi']) +
+        ['wild type']    * len(data_dict['WT_Contra'])
     ),
     'laterality': (
-        ['Ipsi'] * len(data_dict['Mecp2_Ipsi']) +
-        ['Contra'] * len(data_dict['Mecp2_Contra']) +
-        ['Ipsi'] * len(data_dict['WT_Ipsi']) +
-        ['Contra'] * len(data_dict['WT_Contra'])
+        ['ipsilateral'] * len(data_dict['Mecp2_Ipsi']) +
+        ['contralateral'] * len(data_dict['Mecp2_Contra']) +
+        ['ipsilateral'] * len(data_dict['WT_Ipsi']) +
+        ['contralateral'] * len(data_dict['WT_Contra'])
     )
 })
 df_plot = df_plot.dropna(subset=['value'])
@@ -218,4 +226,54 @@ plt.ylim(0, y_max*1.6)
 plt.tight_layout()
 #plt.savefig('jturn_analysis.svg', format='svg', bbox_inches='tight')
 #plt.savefig('jturn_analysis.png', format='png', dpi=100, bbox_inches='tight')
+plt.show()
+
+
+
+###
+def pval_to_star(p):
+    if p <= 0.0001: return "****"
+    if p <= 0.001:  return "***"
+    if p <= 0.01:   return "**"
+    if p <= 0.05:   return "*"
+    return "n.s."
+
+def add_pval_star(ax, x1, x2, y, p_val):
+    text = pval_to_star(p_val)
+    ax.plot([x1, x1, x2, x2], [y, y*1.02, y*1.02, y], lw=1.5, color='black', zorder=4)
+    ax.text((x1 + x2) / 2, y, text, ha='center', va='bottom', fontsize=20)
+
+genotype_palette = {'mecp2-mutant': COLOR_MECP2, 'wild type': COLOR_WT}
+
+plt.figure(figsize=(6, 6))
+
+ax = sns.barplot(
+    data=df_plot,
+    x='laterality',
+    y='value',
+    hue='group',
+    palette=genotype_palette,
+    errorbar='se',    
+    capsize=0.05,      
+    edgecolor='.2', 
+    linewidth=1.5,
+    gap=0.1,
+    err_kws={'zorder': 0}
+)
+
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles[:2], labels[:2], frameon=False)
+
+plt.ylabel('J-turn frequency (Hz)')
+plt.xlabel('')
+
+add_pval_star(ax, -0.2, 0.2,  0.55, p_ipsi_between_bf)
+add_pval_star(ax, -0.2, 0.8, 0.6, p_mecp2_bf)
+add_pval_star(ax, 0.2, 1.2, 0.65, p_wt_bf)
+add_pval_star(ax, 0.8, 1.2, 0.1, p_contra_between_bf)
+
+plt.ylim(0, 0.725)
+plt.tight_layout()
+plt.savefig('jturn_analysis.svg', format='svg', bbox_inches='tight')
+plt.savefig('jturn_analysis.png', format='png', dpi=100, bbox_inches='tight')
 plt.show()
