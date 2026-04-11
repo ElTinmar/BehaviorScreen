1110b8d45cccdd0f7dfe7af0b8c5f82bda8c1080
ElTinmar <martin.privat@hotmail.fr>
2026-04-11 17:02:14 +0200

    update

--- BehaviorScreen/plot_mecp2.py
+++ BehaviorScreen/plot_mecp2.py
@@ -29,7 +29,7 @@ ROOT = Path('/home/martin/Desktop/DATA')
 ROOT = Path('/media/martin/DATA/Behavioral_screen/DATA/Screen')
 ROOT = Path('/media/martin/DATA_18TB/Screen')
 
-groups = ['mecp2/danieau/bouts.csv','AB/danieau/bouts.csv']
+groups = ['mecp2/danieau/bouts.csv','WT/danieau/bouts.csv']
 JTURN = bouts_category_name_short.index('JT')
 prob_threshold = 0.5
 trial_duration_s = 25
@@ -100,224 +100,214 @@ bin_labels = [f"{b[0]}-{b[1]}s" for b in time_bins]
 trial_labels = [f"Trial {i}" for i in range(N_trials)]
 
 # Frequency
-vmax = 0.6
 
-fig, axes = plt.subplots(len(group_names), len(lat_names), 
-                         figsize=(15, 10), sharex=True, sharey=True)
 
-for g_idx in range(len(group_names)):
-    for lat_idx in range(len(lat_names)):
-        ax = axes[g_idx, lat_idx]
-        data = np.nanmean(JT_proba[g_idx, :,lat_idx, :, :], axis=0)
-        
-        sns.heatmap(data, 
-                    annot=True,       
-                    fmt=".3f",        
-                    cmap="magma",     
-                    vmin=0,           
-                    vmax=vmax,        
-                    xticklabels=bin_labels,
-                    yticklabels=trial_labels,
-                    ax=ax,
-                    cbar_kws={'label': '<J-Turn frequency>_fish (Hz)'})
-        
-        ax.set_title(f"Group: {group_names[g_idx]} | {lat_names[lat_idx]}", fontweight='bold')
-        
-        if g_idx == len(group_names) - 1:
-            ax.set_xlabel("Time Bins")
-        if lat_idx == 0:
-            ax.set_ylabel("Trial Number")
 
-plt.tight_layout()
-plt.show()
-
-# PROBA
-vmax = 0.6
-
-fig, axes = plt.subplots(len(group_names), len(lat_names), 
-                         figsize=(15, 10), sharex=True, sharey=True)
-
-for g_idx in range(len(group_names)):
-    for lat_idx in range(len(lat_names)):
-        ax = axes[g_idx, lat_idx]
-        data = np.nanmean(JT_proba[g_idx, :,lat_idx, :, :], axis=0)
-        
-        sns.heatmap(data, 
-                    annot=True,       
-                    fmt=".3f",        
-                    cmap="magma",     
-                    vmin=0,           
-                    vmax=vmax,        
-                    xticklabels=bin_labels,
-                    yticklabels=trial_labels,
-                    ax=ax,
-                    cbar_kws={'label': '<J-Turn probability>_fish'})
-        
-        ax.set_title(f"Group: {group_names[g_idx]} | {lat_names[lat_idx]}", fontweight='bold')
-        
-        if g_idx == len(group_names) - 1:
-            ax.set_xlabel("Time Bins")
-        if lat_idx == 0:
-            ax.set_ylabel("Trial Number")
-
-plt.tight_layout()
-plt.show()
 
 ### German's barplot
 
-data_dict = {
-    'Mecp2_Ipsi':   np.nanmean(JT_freq[0, :, 0, 0:3, 0], axis=1),
-    'Mecp2_Contra': np.nanmean(JT_freq[0, :, 1, 0:3, 0], axis=1),
-    'WT_Ipsi':      np.nanmean(JT_freq[1, :, 0, 0:3, 0], axis=1),
-    'WT_Contra':    np.nanmean(JT_freq[1, :, 1, 0:3, 0], axis=1)
-}
+for data_type, data in [('Frequency (Hz)', JT_freq), ('Probability', JT_proba)]:
 
-p_ipsi_between = mannwhitneyu(
-    data_dict['Mecp2_Ipsi'][~np.isnan(data_dict['Mecp2_Ipsi'])],
-    data_dict['WT_Ipsi'][~np.isnan(data_dict['WT_Ipsi'])]
-).pvalue
+    vmax = 0.6
 
-p_contra_between = mannwhitneyu(
-    data_dict['Mecp2_Contra'][~np.isnan(data_dict['Mecp2_Contra'])],
-    data_dict['WT_Contra'][~np.isnan(data_dict['WT_Contra'])]
-).pvalue
+    fig, axes = plt.subplots(len(group_names), len(lat_names), 
+                            figsize=(15, 10), sharex=True, sharey=True)
 
-# Mecp2 (paired)
-mask_m = ~np.isnan(data_dict['Mecp2_Ipsi']) & ~np.isnan(data_dict['Mecp2_Contra'])
-p_mecp2 = wilcoxon(
-    data_dict['Mecp2_Ipsi'][mask_m],
-    data_dict['Mecp2_Contra'][mask_m]
-).pvalue
+    for g_idx in range(len(group_names)):
+        for lat_idx in range(len(lat_names)):
+            ax = axes[g_idx, lat_idx]
+            data_avg = np.nanmean(data[g_idx, :,lat_idx, :, :], axis=0)
+            
+            sns.heatmap(data_avg, 
+                        annot=True,       
+                        fmt=".3f",        
+                        cmap="magma",     
+                        vmin=0,           
+                        vmax=vmax,        
+                        xticklabels=bin_labels,
+                        yticklabels=trial_labels,
+                        ax=ax,
+                        cbar_kws={'label': f"<J-Turn {data_type}>_fish"})
+            
+            ax.set_title(f"Group: {group_names[g_idx]} | {lat_names[lat_idx]}", fontweight='bold')
+            
+            if g_idx == len(group_names) - 1:
+                ax.set_xlabel("Time Bins")
+            if lat_idx == 0:
+                ax.set_ylabel("Trial Number")
 
-# WT (paired)
-mask_w = ~np.isnan(data_dict['WT_Ipsi']) & ~np.isnan(data_dict['WT_Contra'])
-p_wt = wilcoxon(
-    data_dict['WT_Ipsi'][mask_w],
-    data_dict['WT_Contra'][mask_w]
-).pvalue
+    plt.tight_layout()
+    plt.savefig(f"jturn_{data_type}_heatmap.svg", format='svg', bbox_inches='tight')
+    plt.savefig(f"jturn_{data_type}_heatmap.png", format='png', dpi=100, bbox_inches='tight')
+    plt.show()
 
-pvals = (p_ipsi_between, p_contra_between, p_mecp2, p_wt)
-rejected, (p_ipsi_between_bf, p_contra_between_bf, p_mecp2_bf, p_wt_bf), _, _ = multipletests(pvals, alpha=0.05, method='bonferroni')
+    data_dict = {
+        'Mecp2_Ipsi':   np.nanmean(data[0, :, 0, 0:3, 0], axis=1),
+        'Mecp2_Contra': np.nanmean(data[0, :, 1, 0:3, 0], axis=1),
+        'WT_Ipsi':      np.nanmean(data[1, :, 0, 0:3, 0], axis=1),
+        'WT_Contra':    np.nanmean(data[1, :, 1, 0:3, 0], axis=1)
+    }
 
-df_plot = pd.DataFrame({
-    'value': np.concatenate(list(data_dict.values())),
-    'group': (
-        ['mecp2-mutant'] * len(data_dict['Mecp2_Ipsi']) +
-        ['mecp2-mutant'] * len(data_dict['Mecp2_Contra']) +
-        ['wild type']    * len(data_dict['WT_Ipsi']) +
-        ['wild type']    * len(data_dict['WT_Contra'])
-    ),
-    'laterality': (
-        ['ipsilateral'] * len(data_dict['Mecp2_Ipsi']) +
-        ['contralateral'] * len(data_dict['Mecp2_Contra']) +
-        ['ipsilateral'] * len(data_dict['WT_Ipsi']) +
-        ['contralateral'] * len(data_dict['WT_Contra'])
+    p_ipsi_between = mannwhitneyu(
+        data_dict['Mecp2_Ipsi'][~np.isnan(data_dict['Mecp2_Ipsi'])],
+        data_dict['WT_Ipsi'][~np.isnan(data_dict['WT_Ipsi'])]
+    ).pvalue
+
+    p_contra_between = mannwhitneyu(
+        data_dict['Mecp2_Contra'][~np.isnan(data_dict['Mecp2_Contra'])],
+        data_dict['WT_Contra'][~np.isnan(data_dict['WT_Contra'])]
+    ).pvalue
+
+    # Mecp2 (paired)
+    mask_m = ~np.isnan(data_dict['Mecp2_Ipsi']) & ~np.isnan(data_dict['Mecp2_Contra'])
+    p_mecp2 = wilcoxon(
+        data_dict['Mecp2_Ipsi'][mask_m],
+        data_dict['Mecp2_Contra'][mask_m]
+    ).pvalue
+
+    # WT (paired)
+    mask_w = ~np.isnan(data_dict['WT_Ipsi']) & ~np.isnan(data_dict['WT_Contra'])
+    p_wt = wilcoxon(
+        data_dict['WT_Ipsi'][mask_w],
+        data_dict['WT_Contra'][mask_w]
+    ).pvalue
+
+    pvals = (p_ipsi_between, p_contra_between, p_mecp2, p_wt)
+    rejected, (p_ipsi_between_bf, p_contra_between_bf, p_mecp2_bf, p_wt_bf), _, _ = multipletests(pvals, alpha=0.05, method='bonferroni')
+
+    df_plot = pd.DataFrame({
+        'value': np.concatenate(list(data_dict.values())),
+        'group': (
+            ['mecp2-mutant'] * len(data_dict['Mecp2_Ipsi']) +
+            ['mecp2-mutant'] * len(data_dict['Mecp2_Contra']) +
+            ['wild type']    * len(data_dict['WT_Ipsi']) +
+            ['wild type']    * len(data_dict['WT_Contra'])
+        ),
+        'laterality': (
+            ['ipsilateral'] * len(data_dict['Mecp2_Ipsi']) +
+            ['contralateral'] * len(data_dict['Mecp2_Contra']) +
+            ['ipsilateral'] * len(data_dict['WT_Ipsi']) +
+            ['contralateral'] * len(data_dict['WT_Contra'])
+        )
+    })
+    df_plot = df_plot.dropna(subset=['value'])
+
+    plt.figure(figsize=(6, 6))
+
+    ax = sns.barplot(
+        data=df_plot,
+        x='group',
+        y='value',
+        hue='laterality',
+        palette=['#4C72B0', '#55A868'],
+        errorbar='se',    
+        capsize=0,      
+        alpha=0.9,      
+        edgecolor='.2', 
+        linewidth=1.5,
+        gap=0.1
     )
-})
-df_plot = df_plot.dropna(subset=['value'])
 
-plt.figure(figsize=(6, 6))
+    sns.stripplot(
+        data=df_plot,
+        x='group',
+        y='value',
+        hue='laterality',
+        palette=['#4C72B0', '#55A868'], 
+        jitter=0.15,
+        dodge=True,
+        alpha=0.5,
+        edgecolor='white', 
+        linewidth=1,
+        size=6     
+    )
 
-ax = sns.barplot(
-    data=df_plot,
-    x='group',
-    y='value',
-    hue='laterality',
-    palette=['#4C72B0', '#55A868'],
-    errorbar='se',    
-    capsize=0,      
-    alpha=0.9,      
-    edgecolor='.2', 
-    linewidth=1.5,
-    gap=0.1
-)
+    # Fix legend duplication
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[:2], labels[:2], title='Prey side')
 
-sns.stripplot(
-    data=df_plot,
-    x='group',
-    y='value',
-    hue='laterality',
-    palette=['#4C72B0', '#55A868'], 
-    jitter=0.15,
-    dodge=True,
-    alpha=0.5,
-    edgecolor='white', 
-    linewidth=1,
-    size=6     
-)
+    plt.ylabel(f"J-turn {data_type}")
+    plt.xlabel('')
 
-# Fix legend duplication
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[:2], labels[:2], title='Prey side')
+    def add_pval(ax, x1, x2, y, text):
+        ax.plot([x1, x1, x2, x2], [y, y*1.05, y*1.05, y], lw=1.5, color='black')
+        ax.text((x1 + x2) / 2, y*1.08, text, ha='center', va='bottom')
 
-plt.ylabel('J-turn frequency (Hz)')
-plt.xlabel('')
+    y_max = 0.8* df_plot['value'].max()
 
-def add_pval(ax, x1, x2, y, text):
-    ax.plot([x1, x1, x2, x2], [y, y*1.05, y*1.05, y], lw=1.5, color='black')
-    ax.text((x1 + x2) / 2, y*1.08, text, ha='center', va='bottom')
+    # Positions depend on dodge → approximate:
+    # Mecp2: x=0, WT: x=1
+    # Ipsi ~ -0.2, Contra ~ +0.2 offset
+    add_pval(ax, -0.2, 0.2, y_max*0.85, f"p={p_mecp2_bf:.3e}")
+    add_pval(ax, 0.8, 1.2, y_max*1.25, f"p={p_wt_bf:.3e}")
+    add_pval(ax, -0.2, 0.8, y_max*1.4, f"p={p_ipsi_between_bf:.3e}")
 
-y_max = 0.8* df_plot['value'].max()
-
-# Positions depend on dodge → approximate:
-# Mecp2: x=0, WT: x=1
-# Ipsi ~ -0.2, Contra ~ +0.2 offset
-add_pval(ax, -0.2, 0.2, y_max*0.85, f"p={p_mecp2_bf:.3e}")
-add_pval(ax, 0.8, 1.2, y_max*1.25, f"p={p_wt_bf:.3e}")
-add_pval(ax, -0.2, 0.8, y_max*1.4, f"p={p_ipsi_between_bf:.3e}")
-
-plt.ylim(0, y_max*1.6)
-plt.tight_layout()
-#plt.savefig('jturn_analysis.svg', format='svg', bbox_inches='tight')
-#plt.savefig('jturn_analysis.png', format='png', dpi=100, bbox_inches='tight')
-plt.show()
+    plt.ylim(0, y_max*1.6)
+    plt.tight_layout()
+    #plt.savefig('jturn_analysis.svg', format='svg', bbox_inches='tight')
+    #plt.savefig('jturn_analysis.png', format='png', dpi=100, bbox_inches='tight')
+    plt.show()
 
 
 
-###
-def pval_to_star(p):
-    if p <= 0.0001: return "****"
-    if p <= 0.001:  return "***"
-    if p <= 0.01:   return "**"
-    if p <= 0.05:   return "*"
-    return "n.s."
+    ###
+    def pval_to_star(p):
+        if p <= 0.0001: return "****"
+        if p <= 0.001:  return "***"
+        if p <= 0.01:   return "**"
+        if p <= 0.05:   return "*"
+        return "n.s."
 
-def add_pval_star(ax, x1, x2, y, p_val):
-    text = pval_to_star(p_val)
-    ax.plot([x1, x1, x2, x2], [y, y*1.02, y*1.02, y], lw=1.5, color='black', zorder=4)
-    ax.text((x1 + x2) / 2, y, text, ha='center', va='bottom', fontsize=20)
+    def add_pval_star(ax, x1, x2, y, p_val):
+        text = pval_to_star(p_val)
+        ax.plot([x1, x1, x2, x2], [y, y*1.02, y*1.02, y], lw=1.5, color='black', zorder=4)
+        ax.text((x1 + x2) / 2, y, text, ha='center', va='bottom', fontsize=20)
 
-genotype_palette = {'mecp2-mutant': COLOR_MECP2, 'wild type': COLOR_WT}
+    genotype_palette = {'mecp2-mutant': COLOR_MECP2, 'wild type': COLOR_WT}
 
-plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(6, 6))
 
-ax = sns.barplot(
-    data=df_plot,
-    x='laterality',
-    y='value',
-    hue='group',
-    palette=genotype_palette,
-    errorbar='se',    
-    capsize=0.05,      
-    edgecolor='.2', 
-    linewidth=1.5,
-    gap=0.1,
-    err_kws={'zorder': 0}
-)
+    ax = sns.barplot(
+        data=df_plot,
+        x='laterality',
+        y='value',
+        hue='group',
+        palette=genotype_palette,
+        errorbar='se',    
+        capsize=0.05,      
+        edgecolor='.2', 
+        linewidth=1.5,
+        gap=0.1,
+        err_kws={'zorder': 0}
+    )
 
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[:2], labels[:2], frameon=False)
+    sns.stripplot(
+        data=df_plot,
+        x='laterality',
+        y='value',
+        hue='group',
+        palette=genotype_palette, 
+        jitter=0.15,
+        dodge=True,
+        alpha=0.5,
+        edgecolor='white', 
+        linewidth=1,
+        size=6     
+    )
 
-plt.ylabel('J-turn frequency (Hz)')
-plt.xlabel('')
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[:2], labels[:2], frameon=False)
 
-add_pval_star(ax, -0.2, 0.2,  0.55, p_ipsi_between_bf)
-add_pval_star(ax, -0.2, 0.8, 0.6, p_mecp2_bf)
-add_pval_star(ax, 0.2, 1.2, 0.65, p_wt_bf)
-add_pval_star(ax, 0.8, 1.2, 0.1, p_contra_between_bf)
+    plt.ylabel(f"J-turn {data_type}")
+    plt.xlabel('')
 
-plt.ylim(0, 0.725)
-plt.tight_layout()
-plt.savefig('jturn_analysis.svg', format='svg', bbox_inches='tight')
-plt.savefig('jturn_analysis.png', format='png', dpi=100, bbox_inches='tight')
-plt.show()
+    add_pval_star(ax, -0.2, 0.2,  0.55, p_ipsi_between_bf)
+    add_pval_star(ax, -0.2, 0.8, 0.6, p_mecp2_bf)
+    add_pval_star(ax, 0.2, 1.2, 0.65, p_wt_bf)
+    add_pval_star(ax, 0.8, 1.2, 0.1, p_contra_between_bf)
+
+    plt.ylim(0, 0.725)
+    plt.tight_layout()
+    plt.savefig(f"jturn_{data_type}_comp.svg", format='svg', bbox_inches='tight')
+    plt.savefig(f"jturn_{data_type}_comp.png", format='png', dpi=100, bbox_inches='tight')
+    plt.show()
