b840264c8c921a1548767598ecd3fd23d703bcc2
ElTinmar <martin.privat@hotmail.fr>
2026-04-11 19:30:18 +0200

    dark only

--- BehaviorScreen/plot_mecp2.py
+++ BehaviorScreen/plot_mecp2.py
@@ -33,61 +33,69 @@ ROOT = Path('/media/martin/DATA_18TB/Screen')
 
 groups = ['mecp2/danieau/bouts.csv','WT/danieau/bouts.csv']
 
-# Basic plots ----------------- select dark only
+# Basic plots -----------------
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.kdeplot(df.bout_duration[(df.bout_duration < 1)], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.kdeplot(df_spont.bout_duration[(df_spont.bout_duration < 1)], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.ecdfplot(df.bout_duration[df.bout_duration < 1], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.ecdfplot(df_spont.bout_duration[df_spont.bout_duration < 1], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.kdeplot(df.interbout_duration[df.interbout_duration < 4], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.kdeplot(df_spont.interbout_duration[df_spont.interbout_duration < 4], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.ecdfplot(df.interbout_duration[df.interbout_duration < 4], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.ecdfplot(df_spont.interbout_duration[df_spont.interbout_duration < 4], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.kdeplot(df.peak_yaw_speed, color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.kdeplot(df_spont.peak_yaw_speed, color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.ecdfplot(df.peak_yaw_speed, color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.ecdfplot(df_spont.peak_yaw_speed, color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.kdeplot(df.peak_axial_speed[(df.peak_axial_speed > -50) & (df.peak_axial_speed < 150)], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.kdeplot(df_spont.peak_axial_speed[(df.peak_axial_speed > -50) & (df.peak_axial_speed < 150)], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
 for name, group in zip(['mecp2-mutant', 'wild type'], groups):
     bout_file = ROOT/group
     df = pd.read_csv(bout_file)
-    sns.ecdfplot(df.peak_axial_speed[(df.peak_axial_speed > -50) & (df.peak_axial_speed < 150)], color=genotype_palette[name], label=name)
+    df_spont = df[(df.stim == Stim.DARK) & (df.trial_num>= 10) & (df.trial_num<20)]
+    sns.ecdfplot(df_spont.peak_axial_speed[(df.peak_axial_speed > -50) & (df.peak_axial_speed < 150)], color=genotype_palette[name], label=name)
 plt.legend(frameon=False)
 plt.show()
 
