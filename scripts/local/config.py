cdb3730e0c8a0379e1684addf74d3c79c562325d
ElTinmar <martin.privat@hotmail.fr>
2026-07-10 17:39:09 +0200

    local scripts

--- scripts/local/config.py
+++ scripts/local/config.py
@@ -1,34 +1,15 @@
 from pathlib import Path
 
-#ROOT = Path('/media/martin/DATA1/Behavioral_screen/DATA')
-ROOT = Path('/media/martin/MARTIN_8TB_0/Work/Baier/DATA/Behavioral_screen/DATA')
-
+ROOT = Path('/media/martin/DATA_18TB/Screen')
 CONFIG_YAML = Path('BehaviorScreen/screen.yaml')
 
-FOLDERS = [
-    '1010Kaede-X-81C/ronidazole',
-    '1010Kaede-X-81C/vehicle',
-    'ccka/ronidazole',
-    'ccka/vehicle',
-    'cort/ronidazole',
-    'cort/vehicle',
-    'itpr1b/ronidazole',
-    'itpr1b/vehicle',
-    'lak/danieau',
-    'mafaa/ronidazole',
-    'mafaa/vehicle',
-    'pmch2/ronidazole',
-    'pmch2/vehicle',
-    'pmchl/ronidazole',
-    'pmchl/vehicle',
-    'pth2/ronidazole',
-    'pth2/vehicle',
-    'rspo1/ronidazole',
-    'rspo1/vehicle',
-    'tbr1b/ronidazole',
-    'tbr1b/vehicle',
-    'WT/danieau',
-    'WT/ronidazole',
-    'atf5b/ronidazole',
-    'atf5b/vehicle',
-] 
+def list_subdirectories_depth(root_dir, depth=2):
+    root = Path(root_dir)
+    subdirs = [
+        p for p in root.rglob('*') 
+        if p.is_dir() and len(p.relative_to(root).parts) == depth
+    ]
+    
+    return subdirs
+
+FOLDERS = list_subdirectories_depth(ROOT, 2)
