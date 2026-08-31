e6edeff1d632f03508b58b91f897d76cdc4dba97
ElTinmar <martin.privat@hotmail.fr>
2026-08-31 21:06:54 +0200

    add root

--- scripts/merge_all.sh
+++ scripts/merge_all.sh
@@ -1,1 +1,1 @@
-find /media/martin/DATA_18TB/Screen -name bouts.csv  | xargs python -m BehaviorScreen.merge_csv -o all_bouts.csv
+find /media/martin/DATA_18TB/Screen -name bouts.csv  | xargs python -m BehaviorScreen.merge_csv /media/martin/DATA_18TB/Screen -o all_bouts.csv
