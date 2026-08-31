f577a2c3ceb73972e39b27a1e2338a31dbf7f246
ElTinmar <martin.privat@hotmail.fr>
2026-08-31 15:44:38 +0200

    execpt exception in bootstrap

--- BehaviorScreen/point_process/point_process.py
+++ BehaviorScreen/point_process/point_process.py
@@ -18,7 +18,7 @@ def _fit_single_bootstrap(seed_seq, dataset: PointProcessDataset, model: "PointP
     try:
         model_copy.fit(ds_boot)
         return model_copy.params_
-    except RuntimeError:
+    except Exception:
         return None
 
     
