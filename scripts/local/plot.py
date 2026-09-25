c1c49b3fb8cd4cb7b42ce475fe92ec968892390f
ElTinmar <martin.privat@hotmail.fr>
2026-09-25 20:49:30 +0200

    script

--- scripts/local/plot.py
+++ scripts/local/plot.py
@@ -1,15 +1,33 @@
-from .config import ROOT, FOLDERS, CONFIG_YAML
-from BehaviorScreen.plot import plot_heatmap
-from multiprocessing import Pool, cpu_count
+from .config import FOLDERS, CONFIG_YAML
+from BehaviorScreen.plot import run_plot
+from multiprocessing import Pool
+
+N = 10
 
 def process_folder(folder):
     print(f"processing {folder}")
 
     input_csv = folder / "bouts.csv"
     output_png = folder / "bouts.png"
+    qc_csv = folder / "qc.csv"
 
-    plot_heatmap(input_csv, CONFIG_YAML, output_png)
+    run_plot(
+        qc_csv=qc_csv,
+        bouts_csv=input_csv, 
+        bouts_png=output_png,
+        config_yaml=CONFIG_YAML, 
+        root = folder,
+        metadata = "results",
+        stimuli = "results",
+        tracking = "results",
+        lightning_pose = "lightning_pose",
+        temperature = "results",
+        video = "results",
+        video_timestamp = "results",
+        results = "results",
+        plots = "results",
+    )
     
 if __name__ == "__main__":
-    with Pool(cpu_count()) as pool:
+    with Pool(N) as pool:
         pool.map(process_folder, FOLDERS)
